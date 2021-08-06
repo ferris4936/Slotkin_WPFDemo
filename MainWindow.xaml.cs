2a3a44c59b52cd2c502aa46fa78336106ab8d387
Erica Slotkin <77819164+ferris4936@users.noreply.github.com>
2021-08-06 01:23:52 -0600

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Slotkin_WPFDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void btnStackPanel_Click(object sender, RoutedEventArgs e)
+        {
+            Window window = new StackPanelDemo();
+            window.Show();
+        }
+
+        private void btnWrapPanel_Click(object sender, RoutedEventArgs e)
+        {
+            Window window = new WrapPanelDemo();
+            window.Show();
+        }
+
+        private void btnCanvasPanel_Click(object sender, RoutedEventArgs e)
+        {
+            Window window = new CanvasDemo();
+            window.Show();
+        }
+        private void btnDockPanel_Click(object sender, RoutedEventArgs e)
+        {
+            Window window = new DockPanelDemo();
+            window.Show();
+        }
+        private void btnGridPanel_Click(object sender, RoutedEventArgs e)
+        {
+            Window window = new Grid();  
+            window.Show();
+        }
+    }
+}
